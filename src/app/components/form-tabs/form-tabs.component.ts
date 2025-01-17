25dea3f0981942ea27f3ea1caa85641b877f3565
salskhal <olaniyisal@gmail.com>
2025-01-17 10:52:03 +0100

    🚀 Added more

--- src/app/components/form-tabs/form-tabs.component.ts
+++ src/app/components/form-tabs/form-tabs.component.ts
@@ -6,19 +6,23 @@ import { PersonalDataComponent } from '../personal-data/personal-data.component'
 import { OfferComponent } from '../offer/offer.component';
 import { SummaryComponent } from '../summary/summary.component';
 import { StepperComponent } from '../ui/stepper/stepper.component';
+import { LucideAngularModule, ArrowLeft, ArrowRight } from 'lucide-angular';
 
 
 
 
 @Component({
   selector: 'app-form-tabs',
-  imports: [CommonModule, TermsComponent, CalculatorComponent, PersonalDataComponent, OfferComponent, SummaryComponent, StepperComponent],
+  imports: [CommonModule, TermsComponent, CalculatorComponent, PersonalDataComponent, OfferComponent, SummaryComponent, StepperComponent, LucideAngularModule],
   templateUrl: './form-tabs.component.html',
   styleUrl: './form-tabs.component.css'
 })
 export class FormTabsComponent {
 
-  tabs = ['Terms', 'Calculator', 'Personal Data', 'Offer', 'Summary'];
+  readonly ArrowRight = ArrowRight
+  readonly ArrowLeft = ArrowLeft
+
+  tabs = ['Terms', 'Calculator', 'Form', 'Offer', 'Summary'];
   currentTab = 0;
 
   setCurrentTab(index: number) {
@@ -35,5 +39,17 @@ export class FormTabsComponent {
     if (this.currentTab > 0) {
       this.currentTab--;
     }
+  }
+
+  declineOffer() {
+    console.log('Offer declined.');
+    // Add your logic here (e.g., redirect or show a message)
+    this.previousTab()
+  }
+
+  acceptOffer() {
+    console.log('Offer accepted.');
+    // Add your logic here (e.g., proceed to the next step or finalize process)
+    this.nextTab()
   }
 }
